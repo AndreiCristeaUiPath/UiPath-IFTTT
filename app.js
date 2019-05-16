c5270cc511c2efe1353b306812b73baa58c0df57
Andrei Cristea <andrei.cristea@uipath.com>
2019-05-16 17:34:06 +0300

    Test action and trigger initial impl

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ var iftttChannel = new Ifttt({
 });
 
 // Add triggers & actions to your IFTTT channel.
-iftttChannel.registerTrigger( new (require('./trigger/test'))() );
-iftttChannel.registerAction(  new (require('./action/create'))() );
+iftttChannel.registerTrigger( new (require('./trigger/new_thing_created'))() );
+iftttChannel.registerAction(  new (require('./action/create_new_thing'))() );
 
 iftttChannel.handlers.status = function (request, callback) {
     //fetch('https://yoururl.com/api').then(function (response) {
